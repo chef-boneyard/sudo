c73bb8f58d4fa54ac87375ccd9609fa09932d3a3
jtimberman <joshua@opscode.com>
2012-09-14 16:39:13 -0600

    release v1.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## v1.2.0:
+
+* [COOK-1314] - default package action is now :install instead of :upgrade
+* [COOK-1549] - Preserve SSH agent credentials upon sudo using an attribute
+
 ## v1.1.0:
 
 * [COOK-350] - LWRP to manage sudo files via includedir (/etc/sudoers.d)
--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs sudo and configures /etc/sudoers"
-version           "1.1.1"
+version           "1.2.0"
 
 recipe "sudo", "Installs sudo and configures /etc/sudoers"
 
