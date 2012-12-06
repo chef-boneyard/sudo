ec6356e1c2484ee1e30c2ef3e9431cf32f0fcc91
jtimberman <joshua@opscode.com>
2012-12-06 16:41:12 -0700

    [COOK-1892] - update test kitchen support
    * preflight_command doesn't work in a cookbooks directory with certain
      other cookbooks, but keep the correct command for adding
      ChefSpec (rspec).
    * Add a test cookbook, including LWRP usage test in cook-1892 recipe
    * Move the minitest tests to the test cookbook
    * Update kitchenfile for configurations

--- files/default/tests/minitest/default_test.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path '../support/helpers', __FILE__
-
-describe 'sudo::default' do
-  include Helpers::Sudo
-
-  it 'installs sudo' do
-    package('sudo').must_be_installed
-  end
-end
