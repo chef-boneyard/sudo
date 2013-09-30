fc9bd4aa1276313c680c0346618a49eaf55f9c11
Denis Barishev <denz@twiket.com>
2013-09-30 15:53:20 -0400

    [COOK-3653] Change template attribute to `kind_of` String
    Signed-off-by: Seth Vargo <sethvargo@gmail.com>

--- resources/default.rb
+++ resources/default.rb
@@ -26,7 +26,7 @@ attribute :commands,   :kind_of => Array,           :default => ['ALL']
 attribute :host,       :kind_of => String,          :default => 'ALL'
 attribute :runas,      :kind_of => String,          :default => 'ALL'
 attribute :nopasswd,   :equal_to => [true, false],  :default => false
-attribute :template,   :regex => /^[a-z_]+.erb$/,   :default => nil
+attribute :template,   :kind_of => String,          :default => nil
 attribute :variables,  :kind_of => Hash,            :default => nil
 
 # Set default for the supports attribute in initializer since it is
