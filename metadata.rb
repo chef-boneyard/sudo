2a6af798cbee44a06a3a51b422e1287e278c1ad1
Tim Smith <tsmith84@gmail.com>
2015-09-08 10:06:14 -0700

    Add long_description and supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
 license           'Apache 2.0'
 description       'Installs sudo and configures /etc/sudoers'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '2.7.2'
 
 recipe 'sudo', 'Installs sudo and configures /etc/sudoers'
@@ -10,6 +11,9 @@ recipe 'sudo', 'Installs sudo and configures /etc/sudoers'
 %w(redhat centos fedora ubuntu debian freebsd mac_os_x).each do |os|
   supports os
 end
+
+source_url 'https://github.com/chef-cookbooks/sudo' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/sudo/issues' if respond_to?(:issues_url)
 
 attribute 'authorization',
   :display_name => 'Authorization',
