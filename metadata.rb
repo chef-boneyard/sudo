3b839cddc11fd6168b4069e9f2ffa73eea4d076d
Tim Smith <tsmith@chef.io>
2016-08-04 13:39:14 -0700

    Add chef_version metadata Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -15,3 +15,4 @@ end
 source_url 'https://github.com/chef-cookbooks/sudo' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/sudo/issues' if respond_to?(:issues_url)
 
+chef_version '>= 11' if respond_to?(:chef_version)
