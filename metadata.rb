0f303a07803eecb36dd41bec6e65ebdb9f8ecd26
Tim Smith <tsmith@chef.io>
2017-03-21 16:02:34 -0700

    Update apache2 license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'sudo'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs sudo and configures /etc/sudoers'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '3.3.1'
