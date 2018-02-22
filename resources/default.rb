ed98c4a512af6475dc7d3088b44d17571f811729
Tim Smith <tsmith@chef.io>
2018-02-22 09:49:15 -0800

    Convert to a custom resource with a new config_prefix property
    Don't use a node attribute to store the config_prefix information. Do it with the same case statement, but allow the user to override it.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/default.rb
+++ resources/default.rb
@@ -1,9 +1,13 @@
 #
-# Author:: Bryan W. Berry (<bryan.berry@gmail.com>)
 # Cookbook:: sudo
 # Resource:: default
 #
-# Copyright:: 2011-2016, Bryan w. Berry
+# Author:: Bryan W. Berry (<bryan.berry@gmail.com>)
+# Author:: Seth Vargo (<sethvargo@gmail.com>)
+#
+# Copyright:: 2011-2018, Bryan w. Berry
+# Copyright:: 2012-2018, Seth Vargo
+# Copyright:: 2015-2018, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,34 +21,172 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-actions :install, :remove
-default_action :install
+property :user, String
+property :group, String
+property :commands, Array, default: ['ALL']
+property :host, String, default: 'ALL'
+property :runas, String, default: 'ALL'
+property :nopasswd, [true, false], default: false
+property :noexec, [true, false], default: false
+property :template, String
+property :variables, [Hash, nil], default: nil
+property :defaults, Array, default: []
+property :command_aliases, Array, default: []
+property :setenv, [true, false], default: false
+property :env_keep_add, Array, default: []
+property :env_keep_subtract, Array, default: []
+property :visudo_path, String
+property :config_prefix, String, default: lazy { config_prefix }
 
-attribute :user,              kind_of: String,           default: nil
-attribute :group,             kind_of: String,           default: nil
-attribute :commands,          kind_of: Array,            default: ['ALL']
-attribute :host,              kind_of: String,           default: 'ALL'
-attribute :runas,             kind_of: String,           default: 'ALL'
-attribute :nopasswd,          equal_to: [true, false],   default: false
-attribute :noexec,            equal_to: [true, false],   default: false
-attribute :template,          kind_of: String,           default: nil
-attribute :variables,         kind_of: Hash,             default: nil
-attribute :defaults,          kind_of: Array,            default: []
-attribute :command_aliases,   kind_of: Array,            default: []
-attribute :setenv,            equal_to: [true, false],   default: false
-attribute :env_keep_add,      kind_of: Array,            default: []
-attribute :env_keep_subtract, kind_of: Array,            default: []
-attribute :visudo_path,       kind_of: String,           default: nil
+# default config prefix paths based on platform
+def config_prefix
+  case node['platform_family']
+  when 'smartos', 'freebsd'
+    '/usr/local/etc'
+  else
+    '/etc'
+  end
+end
 
-state_attrs :commands,
-            :group,
-            :host,
-            :nopasswd,
-            :noexec,
-            :runas,
-            :template,
-            :user,
-            :variables,
-            :command_aliases,
-            :env_keep_add,
-            :env_keep_subtract
+# Default action - install a single sudoer
+action :install do
+  target = "#{new_resource.config_prefix}/sudoers.d/"
+
+  package 'sudo' do
+    not_if 'which sudo'
+  end
+
+  unless ::File.exist?(target)
+    sudoers_dir = directory target
+    sudoers_dir.run_action(:create)
+  end
+
+  Chef::Log.warn("#{sudo_filename} will be rendered, but will not take effect because node['authorization']['sudo']['include_sudoers_d'] is set to false!") unless node['authorization']['sudo']['include_sudoers_d']
+  new_resource.updated_by_last_action(true) if render_sudoer
+end
+
+# Removes a user from the sudoers group
+action :remove do
+  resource = file "#{node['authorization']['sudo']['prefix']}/sudoers.d/#{sudo_filename}" do
+    action :nothing
+  end
+  resource.run_action(:delete)
+  new_resource.updated_by_last_action(true) if resource.updated_by_last_action?
+end
+
+action_class do
+  # Ensure that the inputs are valid (we cannot just use the resource for this)
+  def check_inputs(user, group, foreign_template, _foreign_vars)
+    # if group, user, and template are nil, throw an exception
+    if user.nil? && group.nil? && foreign_template.nil?
+      raise 'You must provide a user, group, or template!'
+    elsif !user.nil? && !group.nil? && !template.nil?
+      raise 'You cannot specify user, group, and template!'
+    end
+  end
+
+  # Validate the given resource (template) by writing it out to a file and then
+  # ensuring that file's contents pass `visudo -c`
+  def validate_fragment!(resource)
+    file = Tempfile.new('sudoer')
+
+    begin
+      file.write(capture(resource))
+      file.rewind
+
+      cmd = Mixlib::ShellOut.new("visudo -cf #{file.path}")
+      cmd.environment['PATH'] = "/usr/sbin:#{ENV['PATH']}" if platform_family?('suse')
+      cmd.environment['PATH'] = "/usr/local/sbin:#{ENV['PATH']}" if platform_family?('solaris2')
+      cmd.environment['PATH'] = "#{new_resource.visudo_path}:#{ENV['PATH']}" unless new_resource.visudo_path.nil?
+      cmd.run_command
+      unless cmd.exitstatus == 0
+        Chef::Log.error("Fragment validation failed: \n\n")
+        Chef::Log.error(file.read)
+        Chef::Application.fatal!("Template #{file.path} failed fragment validation!")
+      end
+    ensure
+      file.close
+      file.unlink
+    end
+  end
+
+  # Render a single sudoer template. This method has two modes:
+  #   1. using the :template option - the user can specify a template
+  #      that exists in the local cookbook for writing out the attributes
+  #   2. using the built-in template (recommended) - simply pass the
+  #      desired variables to the method and the correct template will be
+  #      written out for the user
+  def render_sudoer
+    if new_resource.template
+      Chef::Log.debug('Template attribute provided, all other attributes ignored.')
+
+      resource = template "#{node['authorization']['sudo']['prefix']}/sudoers.d/#{sudo_filename}" do
+        source new_resource.template
+        owner 'root'
+        group node['root_group']
+        mode '0440'
+        variables new_resource.variables
+        action :nothing
+      end
+    else
+      sudoer = new_resource.user || ("%#{new_resource.group}".squeeze('%') if new_resource.group)
+
+      resource = template "#{node['authorization']['sudo']['prefix']}/sudoers.d/#{sudo_filename}" do
+        source 'sudoer.erb'
+        cookbook 'sudo'
+        owner 'root'
+        group node['root_group']
+        mode '0440'
+        variables sudoer:             sudoer,
+                  host:               new_resource.host,
+                  runas:              new_resource.runas,
+                  nopasswd:           new_resource.nopasswd,
+                  noexec:             new_resource.noexec,
+                  commands:           new_resource.commands,
+                  command_aliases:    new_resource.command_aliases,
+                  defaults:           new_resource.defaults,
+                  setenv:             new_resource.setenv,
+                  env_keep_add:       new_resource.env_keep_add,
+                  env_keep_subtract:  new_resource.env_keep_subtract
+        action :nothing
+      end
+    end
+
+    # Ensure that, adding this sudoer, would not break sudo
+    validate_fragment!(resource)
+
+    resource.run_action(:create)
+
+    # Return whether the resource was updated so we can notify in the action
+    resource.updated_by_last_action?
+  end
+
+  private
+
+  # acording to the sudo man pages sudo will ignore files in an include dir that have a `.` or `~`
+  # We convert either to `__`
+  def sudo_filename
+    new_resource.name.gsub(/[\.~]/, '__')
+  end
+
+  # Capture a template to a string
+  def capture(template)
+    context = {}
+    context.merge!(template.variables)
+    context[:node] = node
+
+    eruby = Erubis::Eruby.new(::File.read(template_location(template)))
+    eruby.evaluate(context)
+  end
+
+  # Find the template
+  def template_location(template)
+    if template.local
+      template.source
+    else
+      context = template.instance_variable_get('@run_context')
+      cookbook = context.cookbook_collection[template.cookbook || template.cookbook_name]
+      cookbook.preferred_filename_on_disk_location(node, :templates, template.source)
+    end
+  end
+end
