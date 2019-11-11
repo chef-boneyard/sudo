10439ef77c4c429212beb59157cee0cb13318760
Tim Smith <tsmith84@gmail.com>
2019-11-10 18:06:42 -0800

    Remove unnecessary metadata
    No need for any of this anymore
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -2,11 +2,8 @@ name 'sudo'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
-description 'Installs sudo and configures /etc/sudoers'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+description 'Installs sudo and configures /etc/sudoers'\
 version '5.4.4'
-
-recipe 'sudo', 'Installs sudo and configures /etc/sudoers'
 
 %w(aix amazon redhat centos fedora ubuntu debian freebsd mac_os_x oracle scientific zlinux suse opensuse opensuseleap).each do |os|
   supports os
@@ -14,4 +11,4 @@ end
 
 source_url 'https://github.com/chef-cookbooks/sudo'
 issues_url 'https://github.com/chef-cookbooks/sudo/issues'
-chef_version '>= 12.21.3' if respond_to?(:chef_version)
+chef_version '>= 12.21.3'
